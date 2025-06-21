426ceddfe13a681d44b3da5548096bab67d015a6
Abdul Wahid <abdulwahidxhaikh@gmail.com>
2025-06-21 12:46:13 +0500

    some changes

--- smart-student-agent_assistant/main.py
+++ smart-student-agent_assistant/main.py
@@ -36,12 +36,31 @@ print("1. Ask academic question")
 print("2. Get study tip")
 print("3. Summarize text")
 
-user_input = input("Enter Your Question: ")
+choice = input("Enter your choice (1/2/3): ").strip()
 
+if choice == "1":
+    user_input = input("Type your academic question: ")
+    prompt = f"Answer this academic question in a clear, helpful way: {user_input}"
+
+elif choice == "2":
+    prompt = "Give me a useful and practical study tip for students."
+
+elif choice == "3":
+    user_input = input("Paste the text you want summarized: ")
+    prompt = f"Summarize the following text in 2-3 lines: {user_input}"
+
+else:
+    print("❌ Invalid choice.")
+    exit()
+
+# Run agent with chesen prompt
 agent_result = Runner.run_sync(
     student_agent,
-    user_input,
+    prompt,
     run_config=config
 )
 
 print(agent_result.final_output)
+
+
+# --- AbdulWahid ---
